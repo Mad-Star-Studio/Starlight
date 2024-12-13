fe9a300dc54254de209681920659da680aae9302
Seven Of Aces <sevenofaces42@gmail.com>
2024-12-12 19:09:26 -0800

    Initial setup

---
+++ shell.nix
@@ -0,0 +1,7 @@
+let
+  # Pinned nixpkgs, 24.11
+  pkgs = import (fetchTarball("https://github.com/NixOS/nixpkgs/archive/8b27c1239e5c421a2bbc2c65d52e4a6fbf2ff296.tar.gz")) {};
+
+in pkgs.mkShell {
+  buildInputs = [ pkgs.cargo pkgs.rustc pkgs.act ];
+}

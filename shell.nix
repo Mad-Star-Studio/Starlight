1f996cb8b8f221fcc5cae0d31d505df57052c1b9
Seven Of Aces <sevenofaces42@gmail.com>
2024-12-12 19:16:42 -0800

    Updated nix shell file

--- shell.nix
+++ shell.nix
@@ -3,5 +3,14 @@ let
   pkgs = import (fetchTarball("https://github.com/NixOS/nixpkgs/archive/8b27c1239e5c421a2bbc2c65d52e4a6fbf2ff296.tar.gz")) {};
 
 in pkgs.mkShell {
-  buildInputs = [ pkgs.cargo pkgs.rustc pkgs.act ];
+  buildInputs = [ 
+    # Rust tooling
+    pkgs.cargo pkgs.rustc 
+    # Workflow tools
+    pkgs.act
+    # System libraries needed
+    pkgs.pkg-config
+    pkgs.alsa-lib
+    pkgs.udev
+  ];
 }

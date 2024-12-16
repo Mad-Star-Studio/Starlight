bc6490db226a010e5ccbd751499e6681f921999d
TheFelidae <sevenofaces42@gmail.com>
2024-12-16 14:14:07 -0800

    addded udev

--- flake.nix
+++ flake.nix
@@ -22,10 +22,10 @@
       CARGO_INSTALL_ROOT = "${toString ./.}/.cargo";
 
       buildInputs = with pkgs; [ cargo rustc git 
+        udev
         alsa-lib vulkan-loader
         xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr # To use the x11 feature
         libxkbcommon wayland # To use the wayland feature
-        renderdoc 
       ];
       nativeBuildInputs = [ pkgs.pkg-config ];
     };
--- shell.nix
+++ shell.nix
@@ -12,6 +12,7 @@ mkShell rec {
     xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr # To use the x11 feature
     libxkbcommon wayland # To use the wayland feature
     renderdoc
+    wayland
   ];
   LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
 }

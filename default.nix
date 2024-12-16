8c6b85486e1e88ce467a1355c78e2857e69277f3
TheFelidae <sevenofaces42@gmail.com>
2024-12-16 13:35:52 -0800

    add flake

---
+++ default.nix
@@ -0,0 +1,44 @@
+{ pkgs ? import <nixpkgs> { }
+, stdenv ? pkgs.stdenv
+, lib ? stdenv.lib
+# A set providing `buildRustPackage :: attrsets -> derivation`
+, rustPlatform ? pkgs.rustPlatform
+, fetchFromGitHub ? pkgs.fetchFromGitHub
+, gitignoreSrc ? null
+, pkgconfig ? pkgs.pkgconfig
+, gtk3 ? pkgs.gtk3
+, glib ? pkgs.glib
+, gobject-introspection ? pkgs.gobject-introspection
+}:
+
+let
+  gitignoreSource =
+    if gitignoreSrc != null
+    then gitignoreSrc.gitignoreSource
+    else (import (fetchFromGitHub {
+      owner = "hercules-ci";
+      repo = "gitignore";
+      rev = "c4662e662462e7bf3c2a968483478a665d00e717";
+      sha256 = "0jx2x49p438ap6psy8513mc1nnpinmhm8ps0a4ngfms9jmvwrlbi";
+    }) { inherit lib; }).gitignoreSource;
+in
+rustPlatform.buildRustPackage rec {
+  pname = "starlight_engine";
+  version = "0.0.1";
+
+  src = gitignoreSource ./.;
+
+  buildInputs = [
+    udev alsa-lib vulkan-loader
+    xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr # To use the x11 feature
+    libxkbcommon wayland # To use the wayland feature
+    renderdoc
+  ];
+  nativeBuildInputs = [ pkgconfig ];
+
+  meta = with stdenv.lib; {
+    homepage = "";
+    description = "";
+    license = licenses.mit;
+  };
+}
